e03954581c8719c01ff00f63999dfdfbcd67e2d2
Buddhiprakash jain <71218910+Buddhiprakash-jain@users.noreply.github.com>
2022-05-24 14:25:28 +0530

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -4,4 +4,4 @@
 kubectl get deploy &> cmdout.txt
 
 #2. Below cmd compare both files and they match exactly when deploy does not exist and they create deploy and svc both if deploy already exists they release the new version
-diff match.txt cmdout.txt &> /dev/null && kubectl create deploy myd --image=buddhi82/intern:latest;kubectl expose deploy myd --port=80 --type=NodePort || kubectl rollout restart deployment/myd
+diff match.txt cmdout.txt &> /dev/null && kubectl create deploy myd --image=buddhi82/intern:latest;kubectl scale deploy myd --replicas=5;kubectl expose deploy myd --port=80 --type=NodePort || kubectl rollout restart deployment/myd
